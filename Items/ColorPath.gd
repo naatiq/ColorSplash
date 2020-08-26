b5808392ef3568909086d800bc1536d122a2d2d5
naatiq <naatiq@gmail.com>
2020-08-26 22:14:28 +0300

    Refactored ColorPath.gd
    Removed lots of unncessary code, and refactored colorpath.gd

--- Items/ColorPath.gd
+++ Items/ColorPath.gd
@@ -3,18 +3,29 @@ extends Node2D
 
 export var editor_process: = true setget set_editor_process
 
+#Export variables to set the parameters of line and triangle
+
 export var line_color: = Color(0.228943, 0.710254, 0.945312)
 export var line_width: = 3
 export var triangle_color: = Color(0.722656, 0.908997, 1)
 
+
 var _active_point_index: = 0
+# tile map variable stores the parent tile map of colorpath
 var tilemap
+#tiles is an Array of points, that stores the coordinates of tiles
+#On which the colorpath points are located
 var tiles
+#vals is an array to store the index value of each tile
 var vals
 
 
 func _ready() -> void:
-	get_tiles()
+	
+	tilemap = get_parent().get_parent()    #Gets the parent tile map
+	tiles = get_tiles( tilemap )           
+	vals = get_vals(tilemap, tiles)        
+	
 	if not Engine.editor_hint:
 		set_process(false)
 
@@ -22,7 +33,24 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	update()
 
-
+func set_editor_process(value:bool) -> void:
+	editor_process = value
+	if not Engine.editor_hint:
+		return
+	set_process(value)
+	
+func _on_Switch_switch_activated(status):
+	if(status):
+		print("activated")
+		for tile in tiles:
+			tilemap.set_cellv(tile, 4)
+	else:
+		print("deactivated")
+		var count = 0
+		for tile in tiles:
+			tilemap.set_cellv(tile, vals[count])
+			count += 1
+		
 func _draw() -> void:
 	if not Engine.editor_hint:
 		return
@@ -38,26 +66,26 @@ func _draw() -> void:
 			var angle: = last_point.angle_to_point(child.position)
 			triangles.append({center=center, angle=angle})
 		last_point = child.position
-#	points.append(get_child(0).position)
 	
 	draw_polyline(points, line_color, line_width, true)
 	for triangle in triangles:
 		draw_triangle(triangle['center'], triangle['angle'], line_width * 2.0)
 
+#This function returns array of tiles on which path lines are	
+func get_tiles( tilemap ):
+	tiles = PoolVector2Array()
+	for child in get_children():
+		tiles.append(tilemap.world_to_map(child.get_global_position()))
+	return tiles
+	
+#This function returns list of index value for array of tiles
+func get_vals(tilemap, tiles):
+	vals = Array()
+	for tile in tiles:
+		vals.append(tilemap.get_cellv(tile))
+	return vals	
 
-#func get_start_position() -> Vector2:
-#	return get_child(0).global_position
-#
-#
-#func get_current_point_position() -> Vector2:
-#	return get_child(_active_point_index).global_position
-#
-#
-#func get_next_point_position():
-#	_active_point_index = (_active_point_index + 1) % get_child_count()
-#	return get_current_point_position()
-
-
+	
 func draw_triangle(center:Vector2, angle:float, radius:float) -> void:
 	var points: = PoolVector2Array()
 	var colors: = PoolColorArray([triangle_color])
@@ -67,40 +95,3 @@ func draw_triangle(center:Vector2, angle:float, radius:float) -> void:
 		var point: = center + offset
 		points.append(point)
 	draw_polygon(points, colors)
-
-
-func set_editor_process(value:bool) -> void:
-	editor_process = value
-	if not Engine.editor_hint:
-		return
-	set_process(value)
-	
-func _on_Switch_switch_activated(status):
-	if(status):
-		print("activated")
-		change_color(true)
-	else:
-		print("deactivated")
-		change_color(false)
-		
-func get_tiles():
-	tilemap = get_parent().get_parent()
-	tiles = PoolVector2Array()
-	for child in get_children():
-		tiles.append(tilemap.world_to_map(child.get_global_position()))
-	vals = Array()
-	for tile in tiles:
-		vals.append(tilemap.get_cellv(tile))
-	print (vals)	
-
-func change_color(status):
-	if status:
-		for tile in tiles:
-			tilemap.set_cellv(tile, 4)
-	else:
-		var count = 0
-		for tile in tiles:
-			tilemap.set_cellv(tile, vals[count])
-			count += 1
-	
-	
