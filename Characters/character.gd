ace8fb0005b19359b55b825d285d0caa6c78bb01
naatiq <naatiq@gmail.com>
2020-08-31 20:09:18 +0300

    Added Demo Scene

--- Characters/character.gd
+++
@@ -1,8 +0,0 @@
-#Base Class for all characters, used to define export variable 
-
-extends Node2D
-
-#Keep adding enums as new characters are added
-enum CHARACTER_TYPE { EMPTY = -1, PLAYER, ENEMY1 }
-
-export (CHARACTER_TYPE) var type = CHARACTER_TYPE.PLAYER
