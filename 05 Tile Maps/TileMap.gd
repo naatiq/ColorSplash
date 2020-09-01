c68075cfab122ab793ceeb260b03600f9ca7621a
naatiq <naatiq@gmail.com>
2020-09-01 19:02:40 +0300

    Revert "Revert "Revert "Revert "Revert "Removed buttons, added swipe conrrols"""""
    This reverts commit 9615ba28fb3d8ecbdb68109c15684994b9eb66ce.

--- 05 Tile Maps/TileMap.gd
+++ 05 Tile Maps/TileMap.gd
@@ -2,10 +2,13 @@ extends TileMap
 
 
 func _ready():
+	print(cell_size)
 	for child in get_children():
 		#Center each child to the centre of the tile.
 		child.position = map_to_world(world_to_map(child.position))
+		print(str(child.position) + "  " + child.name)
 		child.position += Vector2(cell_size.x/2, cell_size.y/2)
+		print(str(child.position) + "  " + child.name)
 	
 func get_cell_pawn(coordinates):
 	for node in get_children():
@@ -15,6 +18,9 @@ func get_cell_pawn(coordinates):
 func request_move(pawn, direction):
 	var cell_start = world_to_map(pawn.position)
 	var cell_target = cell_start + direction
+	print(cell_start)
+	print(cell_target)
+	print(get_cellv(cell_target))
 	return update_pawn_position(pawn, cell_start, cell_target)
 #		
 			
